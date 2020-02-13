67d74fb88d90f7a7e51f8534c73b9dee337fb3d9
ograsyilmaz <yilmazogras@outlook.com.tr>
2020-02-13 11:28:49 +0300

    js concat eklendi  boşluklar  silindi kod içierisindeki

--- main.js
+++ main.js
@@ -1,50 +1,36 @@
 var url = "https://api.allorigins.win/raw?url=https://www.bloomberght.com/piyasa/intradaydata/dolar";
 var t_tablo = document.getElementById('t-tablo');
 var t_button = document.getElementById('button');
-
 var xhttp = new XMLHttpRequest();
 
-
 var xhttfunc = function () {
-
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
             var salt_data = myArr.SeriesData;
-             if (t_tablo.childElementCount === 10) DeletChild()
+            if (t_tablo.childElementCount === 10) DeletChild()
             for (let index = 0; index < 10; index++) {
-
-
                 var final_date = timeConverter(salt_data[index][0]);
                 var finale_price = salt_data[index][1];
                 var child = document.createElement('tr');
                 var childHtml = tablehtml(final_date, finale_price, index);
                 child.innerHTML = childHtml;
                 t_tablo.appendChild(child);
-
-
             }
         }
     };
     xhttp.open("GET", url, true);
     xhttp.send();
-
-
 };
 
 xhttfunc();
 
-
-
-
-
-function DeletChild(){
+function DeletChild() {
     while (t_tablo.firstChild) {
         t_tablo.removeChild(t_tablo.firstChild);
-      }
-      console.log("Tablo temizlendi Data Eklemeye Hazır")
+    }
+    console.log("Tablo temizlendi Data Eklemeye Hazır")
 }
-
 
 function tablehtml(date, price, no) {
     var contend = "";
@@ -57,22 +43,19 @@ function tablehtml(date, price, no) {
 }
 
 
-
 function timeConverter(timestamp) {
     var a = new Date(timestamp);
+    var humantime = "";
     var months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Agustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'];
-
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
-    return time;
+    humantime.concat(date, ' ', month, ' ', year, ' ', hour, ' : ', min);
+    return humantime;
 }
-
-
 
 t_button.addEventListener("click", function () {
     xhttfunc();
